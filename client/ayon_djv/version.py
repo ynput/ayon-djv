f9a54afdbbdc7dc072bf2ad477d6d8fc27785733
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-04-08 12:39:03 +0200

    updated create package and use package.py instead of manifest

--- client/ayon_djv/version.py
+++ client/ayon_djv/version.py
@@ -1,1 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Package declaring djv addon version."""
 __version__ = "1.0.1-dev.1"
---
+++ package.py
@@ -0,0 +1,6 @@
+name = "djv"
+version = "1.0.1-dev.1"
+title = "DJV"
+client_dir = "ayon_djv"
+
+plugin_for = ["ayon_server"]
